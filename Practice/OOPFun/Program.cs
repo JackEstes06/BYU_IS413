05a7b721538eb4ee59149561e43d2908691743bc
Jack Estes <jackestes@Jacks-MacBook-Pro.local>
2025-01-21 21:55:45 -0700

    Practice

--- Practice/OOPFun/Program.cs
+++ Practice/OOPFun/Program.cs
@@ -1,11 +1,19 @@
-﻿// Enter a quote (from user)
+﻿using OOPFun;
+
+// Enter a quote (from user)
 // How many words
 // How many chars
 // How many of each letter was used
 
+WordTools wt = new WordTools();
 string userQuote = "";
 
 Console.WriteLine("Please enter a quote to analyze: ");
 userQuote = Console.ReadLine();
 
 Console.WriteLine($"Quote: {userQuote}");
+Console.WriteLine($"Word Count: {wt.WordCounter(userQuote)}");
+Console.WriteLine($"Letter Count: {wt.CharCounter(userQuote)}");
+Console.WriteLine($"Letter Map:");
+wt.CharMap(userQuote);
+Console.WriteLine();
--- Practice/OOPFun/WordTools.cs
+++ Practice/OOPFun/WordTools.cs
@@ -4,13 +4,40 @@ public class WordTools
 {
     public int WordCounter(string words)
     {
-        int wordCount = 0;
-        
-        foreach (string word in words.Split(' '))
+        return words.Split(' ').Count();
+    }
+
+    public int CharCounter(string words)
+    {
+        return words.Split(' ').Sum(word => word.Length);
+    }
+
+    public void CharMap(string words)
+    {
+        // Setup default dictionary with each letter set to a 0 count
+        Dictionary<char, int> map = new Dictionary<char, int>();
+        for (char letter = 'A'; letter <= 'Z'; letter++)
         {
-            wordCount++;
+            map[letter] = 0;
         }
 
-        return wordCount;
+        // Loop through the entire string of words and update the dictionary value
+        for (int i = 0; i < words.Length; i++)
+        {
+            char letter = char.ToUpper(words[i]);
+            if (letter >= 'A' && letter <= 'Z')
+            {
+                map[letter]++;
+            }
+        }
+        
+        // Print the dictionary (optional, to verify)
+        foreach (KeyValuePair<char,int> item in map)
+        {
+            Console.WriteLine($"{item.Key}: {item.Value}");
+        }
+
+        // return the map
+        // return map;
     }
 }
